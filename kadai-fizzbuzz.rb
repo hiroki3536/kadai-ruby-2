7726dc71e84430aee87f2d54fdb4c8812f3b61f4
hiroki3536 <85432879+hiroki3536@users.noreply.github.com>
2021-06-08 22:35:54 +0900

    Update kadai-fizzbuzz.rb

--- kadai-fizzbuzz.rb
+++ kadai-fizzbuzz.rb
@@ -1,16 +1,16 @@
 def fizzbuzz(num)
-    if num % 15 == 0 
-     puts "fizzBuzz"
-    elsif num % 3 == 0
-     puts "fizz"
-    elsif num % 5 == 0
-     puts "buzz"
-    else 
-     puts num
-    end
-    return 
+  if num % 15 == 0 
+    "FizzBuzz"
+  elsif num % 3 == 0
+    "Fizz"
+  elsif num % 5 == 0
+    "Buzz"
+  else 
+    num
+  end
 end
+
 num_max = 100
 (1..num_max).each do |num|
-  fizzbuzz(num)
-end
+  puts fizzbuzz(num)
+end 
